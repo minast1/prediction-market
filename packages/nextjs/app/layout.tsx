db48a2873f172cc3144fb49d58a7d95089192eec
minast1 <edmarfo2@gmail.com>
2026-03-11 17:19:15 +0000

    fix: update app title and description

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -6,8 +6,8 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Scaffold-ETH 2 App",
-  description: "Built with 🏗 Scaffold-ETH 2",
+  title: "PredictX",
+  description: "Live Prediction Marketplace.Built with 🏗 Scaffold-ETH 2",
 });
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
